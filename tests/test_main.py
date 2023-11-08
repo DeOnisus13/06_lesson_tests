0d444c10c46aba7b7ce41228e1f7c788f852e941
Denis Antropov <de94onisus@gmail.com>
2023-11-09 02:16:45 +0400

    Add tests for main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,15 +1,24 @@
 import pytest
 from src.main import calculate_taxes
 
-# @pytest.mark.parametrize("price, tax, expect", [(100, 10, 110), (50, 5, 52.5)])
-# def test_calculate_taxes(price, tax, expect):
-#     assert calculate_taxes(price, tax) == expect
-
 
 @pytest.fixture
-def coll():
-    return [100, 50, 200], 10
+def prices():
+    return [100, 50, 200]
 
 
-def test_calculate_taxes(coll):
-    assert calculate_taxes(coll) == [110, 55, 220]
+@pytest.mark.parametrize("tax, expect", [(0, [100, 50, 200]),
+                                         (10, [110, 55, 220])
+                                         ])
+def test_calculate_taxes(prices, tax, expect):
+    assert calculate_taxes(prices, tax) == expect
+
+
+def test_calculate_taxes_invalid_tax(prices):
+    with pytest.raises(ValueError):
+        calculate_taxes(prices, -3)
+
+
+def test_calculate_taxes_invalid_price():
+    with pytest.raises(ValueError):
+        calculate_taxes([-100, 50], 10)
