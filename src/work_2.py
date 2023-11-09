9e835fd6c299ab8094252542ce8b42303b4a0eb0
Denis Antropov <de94onisus@gmail.com>
2023-11-10 01:12:49 +0400

    Add more tests for work_2.py and more functions

--- src/work_2.py
+++ src/work_2.py
@@ -1,7 +1,16 @@
-def calculate_tax(price, tax_rate):
+def calculate_tax(price, tax_rate, *, sale=0, round_=2):
+
+    for arg in (price, tax_rate, sale, round_):
+        if not isinstance(arg, (int, float)):
+            raise TypeError("Это не число")
+
     if price < 0:
         raise ValueError("Неверная цена")
+
     elif tax_rate < 0 or tax_rate >= 100:
         raise ValueError("Неверный налоговый процент")
-    total = price + (price * tax_rate / 100)
-    return total
+
+    tax = price * tax_rate / 100
+    total = price + tax
+    total_sale = total - (total * sale / 100)
+    return round(total_sale, round_)
--- tests/test_work_2.py
+++ tests/test_work_2.py
@@ -12,3 +12,28 @@ def test_calculate_tax_incorrect():
         calculate_tax(-100, 10)
     with pytest.raises(ValueError):
         calculate_tax(100, -3)
+
+
+@pytest.mark.parametrize("sale, expect", [(0, 110), (10, 99), (100, 0)])
+def test_calculate_tax_sale(sale, expect):
+    assert calculate_tax(100, 10, sale=sale) == expect
+
+
+@pytest.mark.parametrize("round_, expect", [(0, 99), (1, 99.4), (2, 99.42), (3, 99.425)])
+def test_calculate_tax_round(round_, expect):
+    assert calculate_tax(100, 2.5, sale=3, round_=round_) == expect
+
+
+@pytest.mark.parametrize("price, tax_rate, sale, round_", [("100", 10, 20, 1),
+                                                           (100, "10", 20, 1),
+                                                           (100, 10, "20", 1),
+                                                           (100, 10, 20, "1")
+                                                           ])
+def test_calculate_tax_check_type(price, tax_rate, sale, round_):
+    with pytest.raises(TypeError):
+        calculate_tax(price, tax_rate, sale=sale, round_=round_)
+
+
+def test_calculate_tax_kwargs():
+    with pytest.raises(TypeError):
+        calculate_tax(100, 10, 5, 2)
